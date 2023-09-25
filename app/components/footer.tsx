d12c128bffda69b609983e348183636b1730e6c5
Patryck Dias Delfes <delfesne@gmail.com>
2023-09-24 22:08:03 -0300

    aa

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,13 +1,62 @@
 import Image from "next/image"
 
 export default function Footer() {
-  return ( 
-    
-    <footer className="h-[10vh] bg-[url('/bg.png')]">
+  return (
 
-      SMNBVSHDVHSDVJHV 
+    <footer className="">
       
+      <div className="">
+
+        <Image className=' w-[100%]'
+          src="/background.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+
+      </div>
+
+      <div className="flex justify-between px-40">
+
+        <div>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+          <ul>
+            Contato
+          </ul>
+        </div>
+
+      </div>
+
     </footer>
 
-  ) 
+  )
 }
